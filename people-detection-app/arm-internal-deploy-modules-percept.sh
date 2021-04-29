9f57050e560a4f2403c157dc67e3081ed92ff485
Micheleen Harris <michhar@microsoft.com>
2021-04-29 12:38:43 -0700

    Return to original deployment manifest for lva

--- people-detection-app/arm-internal-deploy-modules-percept.sh
+++ people-detection-app/arm-internal-deploy-modules-percept.sh
@@ -27,9 +27,10 @@ echo "$(info) Downloading LVA deployment files"
 
 wget "https://raw.githubusercontent.com/microsoft/Azure-Percept-Reference-Solutions/main/people-detection-app/lva-topology-params.json" || true
 wget "https://raw.githubusercontent.com/microsoft/Azure-Percept-Reference-Solutions/main/people-detection-app/lva-topology.json" || true
-wget "https://raw.githubusercontent.com/michhar/Azure-Percept-Reference-Solutions/michhar/test-arm/people-detection-app/deployment.peoplecounting.template.json" || true
-# wget "https://raw.githubusercontent.com/microsoft/Azure-Percept-Reference-Solutions/main/people-detection-app/.env"
-wget "https://raw.githubusercontent.com/michhar/Azure-Percept-Reference-Solutions/michhar/test-arm/people-detection-app/.env" # temp
+# wget "https://raw.githubusercontent.com/michhar/Azure-Percept-Reference-Solutions/michhar/test-arm/people-detection-app/deployment.peoplecounting.template.json" || true
+wget "https://raw.githubusercontent.com/microsoft/Azure-Percept-Reference-Solutions/main/people-detection-app/lva.template.json"
+wget "https://raw.githubusercontent.com/microsoft/Azure-Percept-Reference-Solutions/main/people-detection-app/.env"
+# wget "https://raw.githubusercontent.com/michhar/Azure-Percept-Reference-Solutions/michhar/test-arm/people-detection-app/.env" # temp
 
 echo "$(info) Creating a Python virtual environment and updating pip"
 python3 -m venv /opt/pyenv
@@ -81,7 +82,7 @@ fi
 echo "$(info) Retrieving Edge Device connection string"
 EDGE_DEVICE_CONNECTION_STRING=$(az iot hub device-identity connection-string show --device-id "$DEVICE_NAME" --hub-name "$IOTHUB_NAME" --query "connectionString" -o tsv)
 
-MANIFEST_TEMPLATE_NAME="deployment.peoplecounting.template.json"
+MANIFEST_TEMPLATE_NAME="lva.template.json"
 MANIFEST_ENVIRONMENT_VARIABLES_FILENAME='.env'
 
 echo "$(info) Updating variable values in environment file"
